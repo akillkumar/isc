058ae17c262eeabd9ed0c56754cd13c563f40bc9
Akhil Kumar <akhil@leen.dev>
2024-02-29 14:11:02 +0530

    Add image

--- .next/static/R8S5tvFyo3e2YDf2oxJME/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/594-c45e7890508ec834.js","static/chunks/pages/index-ebfc038dec5cb9ae.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
